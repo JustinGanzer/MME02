cf89f426497128cafa20be72576c63724bcbc163
JustinGanzer <fleischgurke@gmail.com>
2016-04-14 10:44:54 +0200

    js namespace
    ^^

--- Ü1_Sibilak_Helmreich_814618_814650/js/app.js
+++ Ü1_Sibilak_Helmreich_814618_814650/js/app.js
@@ -7,61 +7,62 @@
  * is executed once it matches "complete".
  */
 document.onreadystatechange = function(){
-    if(document.readyState === 'complete'){
-        
-
-        initialize();
-    }
-}
+	if(document.readyState === 'complete'){
 
 
-//initialize-function zur besseren Übersicht
-function initialize(){
-
-	//hole die Controller
-	var controller = document.getElementsByClassName("controller");
-
-	//iteriere über alle Controller
-	for (var i = 0; i < controller.length; i++){
-
-		//hole das zum Controller zugehörige Video und merke es
-		var video = controller[i].parentElement.children[0];
-		controller[i].children[0].video = video;
-        controller[i].children[1].video = video;
-
-        video.onended = function(){
-        	console.log("video ended");
-        	this.parentElement.children[1].children[0].innerHTML="play_arrow";
-        }
-
-		//EventListener hinzufügen für Play/Pause
-		controller[i].children[0].addEventListener("click", function(){
-
-			//Überpüfe, ob pausiert oder nicht und spiele ab/pausiere
-			if(this.video.paused){
-				this.video.play();
-           		this.innerHTML = "pause";
-           		console.log("video played");
-           	}
-			else{
-				this.video.pause();
-            	this.innerHTML = "play_arrow";
-            	console.log("video paused");
-            }
-		});
-        
-        //EventListener hinzufügen für Stop
-		controller[i].children[1].addEventListener("click", function(){
-            stopVideo(this.video);
-            this.parentElement.children[0].innerHTML = "play_arrow";
-		});
+		u1.initialize();
 	}
 }
 
+var u1 = {
+//initialize-function zur besseren Übersicht
+	initialize : function() {
+
+		//hole die Controller
+		var controller = document.getElementsByClassName("controller");
+
+		//iteriere über alle Controller
+		for (var i = 0; i < controller.length; i++){
+
+			//hole das zum Controller zugehörige Video und merke es
+			var video = controller[i].parentElement.children[0];
+			controller[i].children[0].video = video;
+			controller[i].children[1].video = video;
+
+			video.onended = function(){
+				console.log("video ended");
+				this.parentElement.children[1].children[0].innerHTML="play_arrow";
+			}
+
+			//EventListener hinzufügen für Play/Pause
+			controller[i].children[0].addEventListener("click", function(){
+
+				//Überpüfe, ob pausiert oder nicht und spiele ab/pausiere
+				if(this.video.paused){
+					this.video.play();
+					this.innerHTML = "pause";
+					console.log("video played");
+				}
+				else{
+					this.video.pause();
+					this.innerHTML = "play_arrow";
+					console.log("video paused");
+				}
+			});
+
+			//EventListener hinzufügen für Stop
+			controller[i].children[1].addEventListener("click", function(){
+				u1.stopVideo(this.video);
+				this.parentElement.children[0].innerHTML = "play_arrow";
+			});
+		}
+	},
+
 //Stopt das Video
 //@param video - das Video welches gestoppt werden soll
-function stopVideo(video){
-    video.pause();
-    video.currentTime = 0;
-    console.log("video stopped");
+	stopVideo : function(video){
+		video.pause();
+		video.currentTime = 0;
+		console.log("video stopped");
+	}
 }
