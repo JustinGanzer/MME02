eda525f77b8375f1c93b07193519b38e234ffa95
Vincent Helmreich <vincent@helmreich.me>
2016-06-21 15:40:27 +0200

    aufgabe 1 komplett, aufgabe 2 vorbereitet

--- Ü5_Sibilak_Helmreich_814618_814650/models/videos.js
+++ Ü5_Sibilak_Helmreich_814618_814650/models/videos.js
@@ -2,14 +2,15 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var VideoSchema = new Schema({
-    _id: false,
+    //_id: {type: String, required:true},
     title: {type: String, required: true},
     description: {type: String, default: ""},
     src: {type: String, required: true},
     length: {type: Number, required: true},
-    timestamps: {createdAt: "timestamp", default: Date.now},
-    playcount: {type: Number, default: 0},
-    ranking: {type: Number, default: 0}
+    playcount: {type: Number, default: 0, min:0},
+    ranking: {type: Number, default: 0, min:0}
+},{
+    timestamps: {createdAt: "timestamp"}
 });
 
 module.exports = mongoose.model("Video", VideoSchema);
--- Ü5_Sibilak_Helmreich_814618_814650/routes/videos.js
+++ Ü5_Sibilak_Helmreich_814618_814650/routes/videos.js
@@ -19,25 +19,68 @@ var logger = require('debug')('me2u4:videos');
 // TODO add here your require for your own model file
 
 var videos = express.Router();
+//schema
 
+var mongoose = require("mongoose");
+var db = mongoose.connect("mongodb://localhost:27017/me2");
+var VideoModel = require("../models/videos");
 
 // routes **********************
 videos.route('/')
     .get(function(req, res, next) {
-
-        next();
+    
+        VideoModel.find({},function(err,videos){
+            
+            res.status(200);
+            res.locals.items = videos;
+            res.json(videos);
+        }); 
+        
+        //next();
     })
     .post(function(req,res,next) {
-
-        next();
+        
+        var video = new VideoModel(req.body);
+    
+        video.save(function(err){
+            if(!err){
+                res.status(201).json(video);
+            }else{
+                err.status = 400;
+                err.message += " in fields: "
+                                + Object.getOwnPropertyNames(err.errors);
+                
+            }
+            
+            next(err);
+            
+        });
+    
+        //next();
     });
 
+videos.route("/:id")
+    .get(function(req,res,next){
+        
+    })
+    .post(function(req,res,next){
+    
+    })
+    .put(function(req,res,next){
+    
+    })
+    .patch(function(req,res,next){
+    
+    })
+    .delete(function(req,res,next){
+    
+    });
 
 // this middleware function can be used, if you like or remove it
 videos.use(function(req, res, next){
     // if anything to send has been added to res.locals.items
     if (res.locals.items) {
-        // then we send it as json and remove it
+        // then we send it as json and remove i
         res.json(res.locals.items);
         delete res.locals.items;
     } else {
